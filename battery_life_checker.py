62726212dec06829c8cfb1dffc15d58fcc054f72
Michal Pajtinka <michalpajtinka@gmail.com>
2020-10-11 20:16:14 +0100

    first code

--- battery_life_checker.py
+++ battery_life_checker.py
@@ -1,1 +1,16 @@
-@author: Monika
+#@author: Monika
+
+import psutil
+from plyer import notification
+import time
+
+battery=psutil.sensors_battery()
+while True:
+    percent=battery.percent
+    notification.notify(
+        title="Battery Percentage",
+        message=f"{percent}% battery remaining",
+        timeout=10
+    )
+
+    time.sleep(60*5)
